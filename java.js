ee5daedf9f2ea5273b32b4793aac93a9fab324d4
Nylonstrip <nylonstrip@gmail.com>
2025-03-31 19:33:39 -0400

    Added 3 functions for obtaining choice added the ability to get human choice added the ability to get computer choice added the ability to compare choices in showdown

---
+++ java.js
@@ -0,0 +1,91 @@
+let computerChoice = "none";
+let choiceH = "none";
+let humanChoice = "none"
+let cPoints = 0;
+let hPoints = 0;
+
+function getComputerChoice(number) {
+    let choiceN = Math.floor(Math.random() * number);
+    if (choiceN === 2) {
+        computerChoice = "Paper";
+    }
+    else if (choiceN === 1) {
+        computerChoice = "Rock";
+    }
+    else {
+        computerChoice = "Scissors";
+    }
+    return;
+}
+
+function getHumanChoice() {
+    choiceH = prompt("Rock Paper or Scissors?");
+    humanChoice = choiceH.at(0, 1).toUpperCase(1);
+    if (humanChoice === "S") {
+        choiceH = "Scissors";
+    }
+    else if (humanChoice === "R") {
+        choiceH = "Rock"
+    }
+    else if (humanChoice === "P") {
+        choiceH = "Paper"
+    }
+    else {
+        choiceH = "Some random shit"
+    }
+}
+
+
+function showDown() {
+    while (cPoints < 2) {
+    getHumanChoice();
+    getComputerChoice(3);
+    alert(`The computer picked ${computerChoice}`);
+    alert(`The human picked ${choiceH}`);
+    if (computerChoice === choiceH) {
+        alert("Draw!");
+    } 
+    else if (choiceH === "Scissors") {
+        if (computerChoice === "Paper") {
+            ++hPoints;
+            alert("You Win!");
+        }
+        else {
+            ++cPoints;
+            alert("You Lose!");
+        }
+    }
+    else if (choiceH === "Rock") {
+        if (computerChoice === "Scissors") {
+            ++hPoints;
+            alert("You Win!");
+        }
+        else {
+            ++cPoints;
+            alert("You Lose!");
+        }
+    }
+    else if (choiceH === "Paper") {
+        if (computerChoice === "Rock") {
+            ++hPoints;
+            alert("You Win!");
+        }
+        else {
+            ++cPoints;
+            alert("You Lose!");
+        }
+    }
+    else {
+        alert("Why did you pick that?");
+    }
+    alert(`Computer: ${cPoints}| Human: ${hPoints}`);
+}
+    if (cPoints === 3) {
+        alert("Computer whooped yo butt boi");
+    }
+    else {
+        alert("Congrats i guess, you really put in the work...i suppose");
+    }
+}
+
+showDown();
